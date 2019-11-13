3b463227704c18d430e49f190286de2c731eb171
Brandon Weaver <a7xncob@gmail.com>
2019-11-13 23:57:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,6 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-  
   def get_page
     return Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
@@ -31,5 +19,16 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     }
+  end
+  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
   end
 end
